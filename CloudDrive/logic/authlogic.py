19f7203420d6120265f905fe98168f9cf20aeb83
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-10-22 15:46:21 +0530

    Added Register function in logic

--- CloudDrive/logic/authlogic.py
+++ CloudDrive/logic/authlogic.py
@@ -2,8 +2,11 @@
     AuthLogic class is used to implement user authentication logic
 """
 
-from math import e
+# import modules
+import os
+from dotenv import load_dotenv
 import requests
+load_dotenv(".env")
 
 class AuthLogic():
     """
@@ -18,11 +21,33 @@ class AuthLogic():
         """
         # Hit the login API if fields are not empty
         if username != "" and password != "":
-            resp =  requests.post("http://127.0.0.1:5000/users/login/",
+            url = os.getenv("APP_BASE_URL")+os.getenv("USERS_ENDPOINT")+os.getenv("LOGIN_ENDPOINT")
+            print(url)
+            resp =  requests.post(url,
                         headers={"Content-Type": "application/json"},
                         json={"username": username, "password": password}
                     )
             if resp.status_code == 200:
                 # Login successful
                 print("Login status", resp.json())
-        
+
+    def auth_user_register(name, username, password, cloud_provider, api_key):
+        """
+            This method registers the user.
+            - name: the name of the user
+            - username: the username of the user
+            - password: the password of the user
+            - cloud_provider: the cloud provider of the user
+            - api_key: the api key of the user
+        """
+        # Hit the register API if fields are not empty
+        if name != "" and username != "" and password != "" and cloud_provider != "" and api_key != "":
+            url = os.getenv("APP_BASE_URL")+os.getenv("USERS_ENDPOINT")+os.getenv("REGISTER_ENDPOINT")
+            print(url)
+            resp = requests.post(url,
+                        headers={"Content-Type": "application/json"},
+                        json={"name": name, "username": username, "password": password, "cloud_provider": cloud_provider, "cloud_provider_api_key": api_key}
+                    )
+            if resp.status_code == 200:
+                # Registration successful
+                print("Registration status", resp.json())
