e08317ba11ba83caf56b0860014115b80be141e5
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-10-22 12:58:45 +0530

    Modified Logic to hit the server only if field not empty

---
+++ CloudDrive/logic/authlogic.py
@@ -0,0 +1,28 @@
+"""
+    AuthLogic class is used to implement user authentication logic
+"""
+
+from math import e
+import requests
+
+class AuthLogic():
+    """
+        This class handels the logic behind the auth frame
+        (Login/Register), selecting cloud provider, validating API etc
+    """
+    def auth_user_login(username, password):
+        """
+            This method authenticates the user.
+            - username: the username of the user
+            - password: the password of the user
+        """
+        # Hit the login API if fields are not empty
+        if username != "" and password != "":
+            resp =  requests.post("http://127.0.0.1:5000/users/login/",
+                        headers={"Content-Type": "application/json"},
+                        json={"username": username, "password": password}
+                    )
+            if resp.status_code == 200:
+                # Login successful
+                print("Login status", resp.json())
+        
