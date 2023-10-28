8356578e7084b2fdb24a0547734ece91f808b247
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-10-28 21:09:28 +0530

    Modified codebase

--- CloudDrive/view/homeframe.py
+++ CloudDrive/view/homeframe.py
@@ -65,7 +65,7 @@ class HomeFrame():
         # My Account Label
         user_icon_img = tk.PhotoImage(file=os.getenv("USER_ICON"))
         self.my_account_button = ctk.CTkButton(self.my_account,
-            image=os.getenv("USER_ICON"),
+            image=user_icon_img,
             text="My Account",
             font=(os.getenv("DEFAULT_FONT"), int(os.getenv("DEFAULT_FONT_SIZE"))),
             text_color="white",
