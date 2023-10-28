2a0d47ef547c07711da4cb747be05754a2c43807
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-10-28 21:50:32 +0530

    Modified codebase

--- CloudDrive/view/homeframe.py
+++ CloudDrive/view/homeframe.py
@@ -1,9 +1,8 @@
-from calendar import c
 import os
 from dotenv import load_dotenv
 import tkinter as tk
 import customtkinter as ctk
-from PIL import Image, ImageTk
+from PIL import Image
 load_dotenv(".env")
 
 class HomeFrame():
@@ -74,7 +73,7 @@ class HomeFrame():
             hover_color="#023e8a",
             font=(os.getenv("DEFAULT_FONT"), int(os.getenv("HEADING_FONT6_SIZE"))),
             command=lambda: ()
-        ).pack(side=tk.LEFT, padx=10)
+        ).pack(padx=10)
 
         # # User Username
         # ctk.CTkLabel(self.my_account,
