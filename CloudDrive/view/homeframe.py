f762b7d365013660748ff514cc242084a5a2586a
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-10-28 21:01:40 +0530

    Modified codebase

--- CloudDrive/view/homeframe.py
+++ CloudDrive/view/homeframe.py
@@ -32,7 +32,7 @@ class HomeFrame():
         """
             build() method builds the Home frame.
         """
-        self.frame.configure(fg_color="#023e8a", corner_radius=0)
+        self.frame.configure(fg_color="#020001", corner_radius=0)
         self.build_sidebar()
         self.build_main_content()
 
