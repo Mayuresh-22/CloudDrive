8621409c89b8a3922f16344c0e289cfc464dad7c
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-10-28 21:31:37 +0530

    Modified codebase

--- CloudDrive/view/homeframe.py
+++ CloudDrive/view/homeframe.py
@@ -70,6 +70,8 @@ class HomeFrame():
             image=user_icon_img,
             corner_radius=10,
             text_color="white",
+            fg_color="#023e8a",
+            hover_color="#023e8a",
             font=(os.getenv("DEFAULT_FONT"), int(os.getenv("HEADING_FONT6_SIZE"))),
             command=lambda: print("My Account")
         ).pack(side=tk.LEFT, padx=10)
