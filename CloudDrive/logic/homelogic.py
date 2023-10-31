ba340f8aea845912c9c90c5b176d424d2ff6d1fa
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-01 00:59:40 +0530

    Modified codebase

--- CloudDrive/logic/homelogic.py
+++ CloudDrive/logic/homelogic.py
@@ -1,3 +1,16 @@
 """
     HomeLogic is rhe class that handles the logic for the home page.
 """
+
+class HomeLogic:
+    """
+        This class handles the logic behind the Home fram (after successfull authentication).
+    """
+    def __init__(self):
+        pass
+
+    def get_home_page(self):
+        """
+            Returns the home page.
+        """
+        return "home.html"
