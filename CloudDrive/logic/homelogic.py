76533a1ad85c158497c1df4619670eb54dd22948
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-01 01:09:55 +0530

    Modified codebase

--- CloudDrive/logic/homelogic.py
+++ CloudDrive/logic/homelogic.py
@@ -14,7 +14,7 @@ class HomeLogic:
             This method launches the file explorer of the system.
             Opens the file dialog to select the file.
         """
-        file = ctk.CTKc .askopenfilename(initialdir="/", title="Select a File")
+        file = filedialog.askopenfilename(initialdir="/", title="Select a File")
         
         if file:
             print(file)
