962ee7b61534e2f6debc0f216215c81fa7c087f0
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-04 21:22:11 +0530

    Modified codebase

--- CloudDrive Server/app.py
+++ CloudDrive Server/app.py
@@ -138,7 +138,7 @@ def upload_file():
     elif file is None:
         new_file = FilesDB(file_owner=file_owner, file_name=file_name, 
                            file_size=file_size, file_type=file_type, 
-                           file_url=file_url)
+                           file_url=file_url, file_handle=file_handle, file_status=file_status)
 
 
 
