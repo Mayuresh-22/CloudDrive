5831ec619e564769c7a0556e55519aeebe65b146
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-04 19:38:43 +0530

    Modified codebase

--- CloudDrive/logic/homelogic.py
+++ CloudDrive/logic/homelogic.py
@@ -14,7 +14,7 @@ class HomeLogic:
             - userObj: the user object, which contains the user details
         """
         self.userObj = userObj
-        self.cloudObj = cloudObj
+        self.cloudObj = CloudSetup(userObj.cloud_provider, userObj.api_key)
 
 
     def launch_file_explorer(self):
