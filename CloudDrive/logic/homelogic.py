3817580910471476ccb5b4c0ec8a5f97d64555d8
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-04 16:51:31 +0530

    Modified codebase

--- CloudDrive/logic/homelogic.py
+++ CloudDrive/logic/homelogic.py
@@ -35,5 +35,5 @@ class HomeLogic:
             This method uploads the file to the cloud.
             - file: the file to be uploaded
         """
-        print(CloudSetup.get_cloud_provider())
+        print(CloudSetup.api_key)
 
