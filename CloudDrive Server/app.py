46d76e5181af033bac04b605453221bdfc9ee76f
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-04 23:40:29 +0530

    Modified codebase

--- CloudDrive Server/app.py
+++ CloudDrive Server/app.py
@@ -184,10 +184,6 @@ def upload_file():
             "status" : os.getenv("SUCCESS"),
             "message" : os.getenv("UPLOAD_SUCCESS")
         }
-    return {
-        "status" : os.getenv("FAIL"),
-        "message" : os.getenv("UPLOAD_ERROR")
-    }
 
 
 # fallback route for 404
