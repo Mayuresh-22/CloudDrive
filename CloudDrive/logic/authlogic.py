24520087380baad7932430168ad73d5de795f0c2
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-04 12:37:39 +0530

    Modified codebase

--- CloudDrive/logic/authlogic.py
+++ CloudDrive/logic/authlogic.py
@@ -55,6 +55,7 @@ class AuthLogic():
                             headers={"Content-Type": "application/json"},
                             json={"name": kwargs["name"], "username": kwargs["username"], "password": kwargs["password"], "cloud_provider": kwargs["cloud_provider"], "cloud_provider_api_key": kwargs["api_key"]}
                         )
+                print(resp.json())
                 if resp.status_code == 200:
                     # Redirect to Home frame
                     HomeFrame(parent, current, resp.json())
