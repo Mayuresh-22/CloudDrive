11e48d14b28d0ab05865b7fb35e1d775727e27e0
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-04 16:29:06 +0530

    Modified codebase

--- CloudDrive/logic/cloud.py
+++ CloudDrive/logic/cloud.py
@@ -15,6 +15,8 @@ class CloudSetup():
             Checks the cloud provider and calls the respective method.
         """
         self.client = None
+        self.cloud_provider = cloud_provider
+        self.api_key = api_key
         if cloud_provider == "Filestack":
             self.filestack_setup(api_key)
         else:
