9c4a85089b09b103c12b50c5016aa623082173cf
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-04 16:42:59 +0530

    Modified codebase

--- CloudDrive/logic/homelogic.py
+++ CloudDrive/logic/homelogic.py
@@ -8,13 +8,6 @@ class HomeLogic:
     """
         This class handles the logic behind the Home fram (after successfull authentication).
     """
-    def __init__(self, userObj):
-        """
-            This is the constructor of the HomeLogic class.
-            - userObj: the user object, which contains the user details
-        """
-        CloudSetup.__init__(self, userObj["cloud_provider"], userObj["api_key"])
-        
 
     def launch_file_explorer(self, userObj):
         """
