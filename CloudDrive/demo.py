e2afbfba3e1bff9cce3677d4deabc82de2aa9bff
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-04 22:42:52 +0530

    Modified codebase

--- CloudDrive/demo.py
+++ CloudDrive/demo.py
@@ -21,8 +21,8 @@ policy = {
 security = Security(policy, "7PDJLJSTP5G33M7FKQQE3Q4W3E")
 
 
-# fileinfo = client.upload(filepath="assets/img.jpg", security=security)
-fileinfo = client.upload(filepath="assets/1.pdf")
+fileinfo = client.upload(filepath="assets/img.jpg", security=security)
+# fileinfo = client.upload(filepath="assets/1.pdf")
 # print(fileinfo.upload_response)
 # print(f"URL: https://cdn.filestackcontent.com/{security.as_url_string()}/{fileinfo.upload_response['handle']} \n")
 print(f"URL: https://cdn.filestackcontent.com/{fileinfo.upload_response['handle']} \n")
