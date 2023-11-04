dabcf648fad34c077516734d519d9c2e6d9181df
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-04 22:35:31 +0530

    Modified codebase

--- CloudDrive/logic/homelogic.py
+++ CloudDrive/logic/homelogic.py
@@ -79,9 +79,8 @@ class HomeLogic:
                         "file_url_pub" = filelink.upload_response["file_url_pub"],
                         "file_url_pvt" = filelink.upload_response["file_url_pvt"],
                         "file_handle" = filelink.upload_response["file_handle"],
-                        "file_status" = filelink.upload_response["file_status"],
+                        "file_status" = filelink.upload_response["file_status"]
                     }
-                    
                 )
             except:
                 pass
