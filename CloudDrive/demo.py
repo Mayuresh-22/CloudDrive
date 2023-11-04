6bc518cd0a4eec162e034a86cfe9cd2925d605f7
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-04 15:39:31 +0530

    Modified codebase

--- CloudDrive/demo.py
+++ CloudDrive/demo.py
@@ -27,7 +27,7 @@ print(f"URL: https://cdn.filestackcontent.com/{security.as_url_string()}/{filein
 
 # print file info
 print("\nFile info:\n")
-print(fileinfo.metadata(['size', 'type', 'filename', 'sourceUrl', 'mimetype']))
+print(fileinfo.metadata(['size','filename']))
 
 # Delete the file
 # print("\nDeleting the file...\n")
