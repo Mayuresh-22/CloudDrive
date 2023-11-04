ad8659fba2f9f137e8cebbb928015ee3461e1e2e
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-04 23:59:18 +0530

    Modified codebase

--- CloudDrive Server/app.py
+++ CloudDrive Server/app.py
@@ -151,7 +151,7 @@ def get_all_files():
 # api endpoint to upload file
 @app.route('/files/upload/', methods=['GET', 'POST'])
 def upload_file():
-    if not request.json or not 'file_owner' in request.json or not 'file_name' in request.json or not 'file_size' in request.json or not 'file_type' in request.json or not 'file_url' in request.json or not 'file_handle' in request.json or not 'file_status' in request.json:
+    if not request.json or not 'file_owner' in request.json or not 'file_name' in request.json or not 'file_size' in request.json or not 'file_type' in request.json or not 'file_url_pub' in request.json or not 'file_handle' in request.json or not 'file_status' in request.json:
         return {"status" : os.getenv("FAIL"),
             "message" : os.getenv("MISSING_PARAMETERS")
         }
