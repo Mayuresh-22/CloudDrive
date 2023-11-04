621b33b14774811547573e77e55641b8c9b25673
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-04 20:07:07 +0530

    Modified codebase

--- CloudDrive/logic/homelogic.py
+++ CloudDrive/logic/homelogic.py
@@ -1,16 +1,5 @@
 """
     HomeLogic is rhe class that handles the logic for the home page.
-    This class __init__ method takes the user object as the parameter.
-    This class has the following methods:
-    - launch_file_explorer(): This method launches the file explorer of the system.
-    - upload_file(): This method is the global method to upload the file to the cloud.
-
-    This class has the following attributes:
-    - userObj: the user object, which contains the user details (JSON)
-    - cloudSetObj: the cloud provider setup object (CloudSetup)
-    - filehandler: the file handler object for the cloud provider,
-                   which handles the file actions (Any cloud provider type)
-
 """
 
 # Import the required modules
