cd0002d4445572ef58f4d7eeef705cef340dc75f
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-04 16:23:33 +0530

    Modified codebase

--- CloudDrive/logic/cloud.py
+++ CloudDrive/logic/cloud.py
@@ -8,7 +8,7 @@ class CloudSetup():
     """
         This class contains the logic for the cloud cloud provider setup.
     """
-    
+
     def __init__(self, cloud_provider, api_key):
         """
             This is the constructor for the class.
@@ -22,6 +22,6 @@ class CloudSetup():
     
     def filestack_setup(self, api_key, **kargs):
         # Initialize the client
-        client = Client("api_key")
+        self.client = Client("api_key")
 
         
