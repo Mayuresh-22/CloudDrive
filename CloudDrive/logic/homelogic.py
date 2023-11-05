af1f31e486079af53aa562fb4ecaeb73d6ca6906
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-05 11:48:47 +0530

    Modified codebase

--- CloudDrive/logic/homelogic.py
+++ CloudDrive/logic/homelogic.py
@@ -109,6 +109,8 @@ class HomeLogic:
             )
             if resp.status_code == 200 and resp.json()["status"] == "success":
                 files = resp.json()["files"]
+                row = 0
+                column = 0
                 for file in files:
                     file_frame = ctk.CTkFrame(files_frame,
                         width=200,
