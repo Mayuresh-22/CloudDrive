d9341497e951bea1a031667adf144d2d4ccc025b
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-05 16:54:36 +0530

    Modified codebase

--- CloudDrive/logic/homelogic.py
+++ CloudDrive/logic/homelogic.py
@@ -238,6 +238,6 @@ class HomeLogic:
 
         
     def download_file(self, file_url, file_name):
-        print("File URL: ", file_url)
+        print("File URL: ", file_url.cget("text")
         print("File Name: ", file_name)
 
