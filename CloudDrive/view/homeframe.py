24c94cd7c08a66c38a36c534fbaacef37153bcf1
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-05 12:16:17 +0530

    Modified codebase

--- CloudDrive/view/homeframe.py
+++ CloudDrive/view/homeframe.py
@@ -145,7 +145,7 @@ class HomeFrame():
         ).pack(pady=0, padx=30, side=tk.TOP, anchor=tk.W)
 
         # Main Content - Files (grid view)
-        files = ctk.CTkScrollableFrame(master=self.main_content,
+        self.files = ctk.CTkScrollableFrame(master=self.main_content,
             width=int(os.getenv("DEFAULT_APP_WIDTH"))*0.8,
             height=int(os.getenv("DEFAULT_APP_HEIGHT"))*0.7,
             fg_color="#ffffff",
