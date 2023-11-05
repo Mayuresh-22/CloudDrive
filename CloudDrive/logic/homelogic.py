53d12fa313334078f39d9ceca935a60f2123bade
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-05 11:58:46 +0530

    Modified codebase

--- CloudDrive/logic/homelogic.py
+++ CloudDrive/logic/homelogic.py
@@ -120,7 +120,7 @@ class HomeLogic:
                         fg_color="#E3F5FD",
                         corner_radius=10
                     )
-                    file_frame.grid(row=row, column=col, padx=10, pady=10, side = tk.TOP)
+                    file_frame.grid(row=row, column=col, padx=10, pady=10)
                     file_frame.pack_propagate(False)
                     file_frame.grid_propagate(False)
 
