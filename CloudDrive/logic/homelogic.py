783f951a48008798f2df5a840087733a617858c5
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-05 12:53:54 +0530

    Modified codebase

--- CloudDrive/logic/homelogic.py
+++ CloudDrive/logic/homelogic.py
@@ -144,6 +144,7 @@ class HomeLogic:
                     file_icon = ctk.CTkImage(Image.open(self.file_icons[file_type]), size=(50, 50))
                     ctk.CTkLabel(file_frame,
                         image=file_icon,
+                        text=""
                         fg_color="#E3F5FD",
                         bg_color="#E3F5FD",
                     ).pack(pady=5, side = tk.TOP)
