7d615b8b5a7ec75081264497f1901c06d3ea75a8
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-05 17:16:16 +0530

    Modified codebase

--- CloudDrive/logic/homelogic.py
+++ CloudDrive/logic/homelogic.py
@@ -158,6 +158,7 @@ class HomeLogic:
                     text_color="black",
                     font=(os.getenv("DEFAULT_FONT"), int(os.getenv("HEADING_FONT5_SIZE"), 10))
                     ).pack(pady=30)
+                    return
                 row = 0
                 col = 0
                 max_col = 4
