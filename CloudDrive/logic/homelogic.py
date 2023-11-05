ca2256f6632f0843757b55b0b3403a85d0c72741
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-05 11:16:25 +0530

    Modified codebase

--- CloudDrive/logic/homelogic.py
+++ CloudDrive/logic/homelogic.py
@@ -93,4 +93,5 @@ class HomeLogic:
             - files_frame: the frame to populate the files
 
             This method calls the get_files() method of the respective cloud provider.
+            And, populates the files in the files frame.
     """
