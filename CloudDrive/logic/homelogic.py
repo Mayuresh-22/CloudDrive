9df2c23a5e8a1499246f004f840024225389a4d6
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-05 11:59:38 +0530

    Modified codebase

--- CloudDrive/logic/homelogic.py
+++ CloudDrive/logic/homelogic.py
@@ -135,7 +135,7 @@ class HomeLogic:
                         text='Hello',
                         fg_color="black",
                         font=(os.getenv("DEFAULT_FONT"), int(os.getenv("HEADING_FONT6_SIZE"), 10))
-                    )
+                    ).pack(pady=5)
 
                     col += 1
                     if col == max_col:
