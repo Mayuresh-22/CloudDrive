ac0892aa803f099caf78986ff45a2d38f48b1ca4
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-05 11:17:46 +0530

    Modified codebase

--- CloudDrive/logic/homelogic.py
+++ CloudDrive/logic/homelogic.py
@@ -92,8 +92,8 @@ class HomeLogic:
             This method populates the files in the files frame.
             - files_frame: the frame to populate the files
 
-            This method calls the get_files() method of the respective cloud provider.
-            And, populates the files in the files frame.
+            This method sends POST request to the server to get the files of the user.
+            and populates the files in the files frame.
     """
         # Get the files from the cloud provider
         files = self.cloud.get_files()
