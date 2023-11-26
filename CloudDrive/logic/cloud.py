2ea9a8767bd9d08fd51bc2ba41749578d05016e5
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-26 19:25:42 +0530

    Modified codebase

--- CloudDrive/logic/cloud.py
+++ CloudDrive/logic/cloud.py
@@ -7,7 +7,7 @@ from filestack import Client
 import os
 from dotenv import load_dotenv
 import requests
-import homelogic
+from homelogic import HomeLogic
 load_dotenv(".env")
 
 class CloudSetup():
