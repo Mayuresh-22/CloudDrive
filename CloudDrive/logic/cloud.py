dd3642f6cceec7c0c7a0324432f4b41b00caaa34
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-11-26 19:37:04 +0530

    Modified codebase

--- CloudDrive/logic/cloud.py
+++ CloudDrive/logic/cloud.py
@@ -1,13 +1,10 @@
 """
     This file contains the logic for the cloud cloud provider setup.
 """
+from re import T
 from tkinter import filedialog
 from filestack import Client
 import os
-import customtkinter as ctk
-import tkinter as tk
-from PIL import Image
-from functools import partial
 from dotenv import load_dotenv
 import requests
 load_dotenv(".env")
@@ -26,6 +23,7 @@ class CloudSetup():
         self.cloud_provider = cloud_provider
         self.api_key = api_key
         self.kwargs = kwargs
+
 
     def setup(self, **kwargs):
         """
@@ -53,7 +51,6 @@ class Filestack():
             - cloudSetObj: the cloud provider setup object (CloudSetup)
         """
         self.api_key = api_key
-        self.filelink = None
         self.filetypes = {
             "pdf" : ("PDF files","*.pdf"),
             "docx" : ("DOCX files","*.docx"),
@@ -74,150 +71,14 @@ class Filestack():
         # print("Filestack setup done")
 
 
-    def upload_file(self, file, files_frame):
+    def upload_file(self, file):
         """
             This method uploads the file to the cloud.
             - file: the file to be uploaded
-            - file_frame: the frame where uploaded files are populated
         """
-        self.filelink = self.client.upload(filepath=file)
-
-                # Checking if the file is uploaded successfully
-        if self.filelink.upload_response["status"] == "Stored":
-            """
-                If the file is uploaded successfully,
-                Now, storing the file details in the database.
-            """
-            url = os.getenv("APP_BASE_URL")+os.getenv("FILE_ENDPOINT")+os.getenv("UPLOAD_ENDPOINT")
-            resp = requests.post(url,
-                headers={"Content-Type": "application/json"},
-                json={
-                    "file_owner" : self.userObj["id"],
-                    "cloud_provider_api_key" : self.userObj["cloud_provider_api_key"],
-                    "file_name" : self.filelink.upload_response["filename"],
-                    "file_size" : self.filelink.upload_response["size"],
-                    "file_type" : self.filelink.upload_response["mimetype"],
-                    "file_url_pub" : self.filelink.upload_response["url"],
-                    "file_url_pvt" : "",
-                    "file_handle" : self.filelink.upload_response["handle"],
-                    "file_status" : self.filelink.upload_response["status"]
-                }
-            )
-            if resp.status_code == 200 and resp.json()["status"] == "success":
-                # print(resp.json()["message"])
-                self.populate_files(files_frame)
-            else:
-                # print(resp.json()["message"])
-                pass
+        filelink = self.client.upload(filepath=file)
+        return filelink
     
-
-    def populate_files(self, files_frame, userObj):
-        """
-            This method populates the files in the files frame.
-            - files_frame: the frame to populate the files
-
-            This method sends POST request to the server to get the files of the user.
-            and populates the files in the files frame.
-    """
-        
-        # Clear the files frame
-        for widget in files_frame.winfo_children():
-            widget.destroy()
-
-        # Get the files from the server for the user
-        url = os.getenv("APP_BASE_URL")+os.getenv("FILE_ENDPOINT")
-        try:
-            resp = requests.post(url,
-                headers={"Content-Type": "application/json"},
-                json={
-                    "file_owner" : userObj["id"],
-                    "cloud_provider_api_key" : userObj["cloud_provider_api_key"]
-                }
-            )
-            if resp.status_code == 200 and resp.json()["status"] == "success":
-                files = resp.json()["files"]
-                if len(files) == 0:
-                    no_file_label = ctk.CTkLabel(files_frame,
-                        text="No files found",
-                        fg_color="#E3F5FD",
-                        text_color="#1B387C",
-                        font=(os.getenv("DEFAULT_FONT"), int(os.getenv("HEADING_FONT6_SIZE"), 10))
-                    )
-                    no_file_label.grid(fill=tk.BOTH, expand = True, rowspan=4, columnspan=4, padx=10, pady=10)
-                    no_file_label.pack_propagate(False)
-                    no_file_label.grid_propagate(False)
-                    return None
-                
-                row = 0
-                col = 0
-                max_col = 4
-                for file in files:
-                    file_type = file["file_type"].split("/")[1]
-                    """
-                        Creating the file block
-                        File block contains the file icon and the file name
-                        And, action buttons to download, view, delete the file
-                    """
-                    file_block = ctk.CTkFrame(files_frame,
-                        width=190,
-                        height=190,
-                        fg_color="#E3F5FD",
-                        corner_radius=10
-                    )
-                    file_block.grid(row=row, column=col, padx=10, pady=10)
-                    file_block.pack_propagate(False)
-                    file_block.grid_propagate(False)
-
-                    # file icon
-                    file_icon = ctk.CTkImage(Image.open(self.file_icons[file_type]), size=(55, 55))
-                    ctk.CTkLabel(file_block,
-                        image=file_icon,
-                        text="",
-                        fg_color="#E3F5FD",
-                        bg_color="#E3F5FD",
-                    ).pack(pady=20, side = tk.TOP)
-
-                    # file name
-                    file_name = ctk.CTkLabel(file_block,
-                        text=file["file_name"][0:15]+"...",
-                        fg_color="#E3F5FD",
-                        text_color="#1B387C",
-                        font=(os.getenv("DEFAULT_FONT"), int(os.getenv("HEADING_FONT6_SIZE"), 10))
-                    )
-                    file_name.pack(pady=5)
-
-                    # file actions
-                    file_actions = ctk.CTkFrame(file_block,
-                        width=190,
-                        height=30,
-                        fg_color="#E3F5FD",
-                        corner_radius=10
-                    )
-                    file_actions.pack(pady=10)
-                    file_actions.pack_propagate(False)
-
-                    # download button
-                    download_icon = ctk.CTkImage(Image.open(os.getenv("DOWNLOAD_ICON")), size=(30, 30))
-                    download_button = ctk.CTkButton(file_actions,
-                        width=50,
-                        image=download_icon,
-                        text="",
-                        fg_color="transparent",
-                        bg_color="transparent",
-                        hover=False,
-                        font=(os.getenv("DEFAULT_FONT"), int(os.getenv("HEADING_FONT6_SIZE"), 10)),
-                        command=partial(self.download_file, file["file_url"], file["file_name"])
-                    )
-                    download_button.pack(side=tk.LEFT, padx=5)
-
-                    col += 1
-                    if col == max_col:
-                        row += 1
-                        col = 0
-
-        except Exception as e:
-            print(e)
-
 
     def download_file(self, fileurl, filename):
         """
